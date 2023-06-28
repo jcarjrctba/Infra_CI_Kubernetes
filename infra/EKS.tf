731e322b999e9fabd8ebf568aa1e02a0d859e92b
Junior <114120831+jcarjrctba@users.noreply.github.com>
2023-06-28 10:33:20 -0300

    Update EKS.tf

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -6,7 +6,7 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access  = true
 
-  #create_cni_ipv6_iam_policy = true
+  create_cni_ipv6_iam_policy = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
